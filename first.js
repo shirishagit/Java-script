141483737124b7a99f406d438c3c01aad49d7d6a
shirisha <shirisha.baasani@gmail.com>
2024-12-13 21:31:09 +0530

    dec 13

--- first.js
+++ first.js
@@ -30,10 +30,10 @@ console.log(typeof userid["username"])
 let a = 5;
 let b = 2;
 console. log("a = ", a ," & b = ",b);
-console.log("a + b =",a + b);
-console.log("a - b =",a - b);
-console.log("a * b =",a * b);
-console.log("a / b =",a / b);
+console.log("a + b =",a + b); //7
+console.log("a - b =",a - b); //3
+console.log("a * b =",a * b);//10
+console.log("a / b =",a / b); //2.5
 console.log("a % b =",a % b); //remainder 1
 console.log("a ** b =",a ** b); // 25 a to the power of b
 
