5fa4c61e9fbccffeb96dc09fcd0e413d6303f5e5
shirisha <shirisha.baasani@gmail.com>
2024-12-23 22:21:26 +0530

    arrows

---
+++ FUNCTIONS/one.js
@@ -0,0 +1,18 @@
+function first(a,b) {
+    console.log("sub = " + (a-b))
+return a*b;
+}
+
+const sub =first(30,20);
+console.log(sub)
+
+// calculate sum of 2 numbers
+
+function sum(x,y){            //x and y are the parameters they are block of code on work with in the code 
+   s = x+y;
+   return s;                 // if u ues return value the code stopes working 
+}
+sum(20,30);               //50
+sum(80,90);              // 170
+
+
