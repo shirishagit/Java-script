dc8f63939da48fe307b5c37816e760cb0969b433
shirisha <shirisha.baasani@gmail.com>
2024-12-25 18:26:31 +0530

    one

--- FILTER/one.js
+++ FILTER/one.js
@@ -15,3 +15,18 @@ console.log(output);                          // FILTER FUNCTION TO PRINT ODD NU
 
 const print = items.filter((x)=> x %2 ===0);
 console.log(print);                      // FILTER FUNCTION TO PRINT EVEN NUMBER IN CALL BACK FUNCTION ALSO IN ARROW FUNCTION
+
+
+// practice question 2
+
+
+const students=[
+    {Name:"Shirisha", rollNo:1,age:21},
+    {Name:"Snehitha",rollNo:2,age:20},
+    {Name:"Arjun",rollNo:3,age:20},
+    {Name:"raviteja",rollNo:4,age:20},
+];
+
+
+const list=students.filter((x)=>x.age > 18);
+console.log(list);
