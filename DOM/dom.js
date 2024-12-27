d4c753e24168408fa2fe2bacacfea1d25ea78e1e
shirisha <shirisha.baasani@gmail.com>
2024-12-27 22:29:00 +0530

    select the elements

---
+++ DOM/dom.js
@@ -0,0 +1,18 @@
+ //IF YOU WANT TO ACCESS THE ELEMENT BY ITS ID HERE IS THE EXAMPLE
+ 
+let button=document.getElementById("button");   
+console.dir(button);
+
+
+
+//IF YOU WANT TO ACCESS A GROUP OG ELEMENT WITH SAME FEATURES YOU CAN USE CLASS
+let class1 = document.getElementsByClassName("class1");
+console.dir(class1);
+console.log(class1);
+
+
+// IF YOU WANT TO ACCESS  ELEMENTS WITH  THE TAG NAME
+// YOU CAN GIVE THE TAG NAME AND YOU CAN ACCESS THEM 
+
+let para = document.getElementsByTagName("p");
+console.dir(para);
