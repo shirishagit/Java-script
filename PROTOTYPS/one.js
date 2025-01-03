0b7a0f5b1ac41927a368abd08fb4a2542e2dc67e
shirisha <shirisha.baasani@gmail.com>
2025-01-03 23:09:28 +0530

    new commit

--- PROTOTYPS/one.js
+++ PROTOTYPS/one.js
@@ -1,7 +1,17 @@
-//PROTOTYPES IS AN OBJECT 
+//prototype 
 
 let employee ={
     calcTax(){
         console.log("Tax deducted is 10%")
     }
 };
+
+const manisha = {
+    salary:2000,
+    calcTax(){
+        console.log("tax is 5% extra")
+    },
+};
+
+
+manisha.__protot__ = employee;
