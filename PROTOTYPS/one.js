4ca428bb316c35fc492b77dd3da26a6b8f16be56
shirisha <shirisha.baasani@gmail.com>
2025-01-04 23:24:27 +0530

    new updates

--- PROTOTYPS/one.js
+++ PROTOTYPS/one.js
@@ -1,4 +1,4 @@
-//prototype 
+//PROTOTYPES
 
 let employee ={
     calcTax(){
@@ -14,4 +14,23 @@ const manisha = {
 };
 
 
-manisha.__protot__ = employee;
+
+
+//CLASSESS
+
+class companies{
+    constructor(Name,ceo,year,field){
+      this.Name = Name;
+      this.ceo = ceo;
+      this.year = year;
+      this.field = field;
+    }
+    changefield(cattegery){
+        this.field = cattegery
+    }
+};
+
+
+let Company1 = new companies("google","sundar pachai","1980","Product");
+Company1.changefield("service");  // IF YOU WANT TO MAKE ANY CHANGE IN THE ABOVE METHODS  
+console.log(Company1);
